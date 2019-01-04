3f23d32ae9ae1b5b407123c1eafac36d5285de48
François Pluchino <francois.pluchino@gmail.com>
2019-01-04 12:24:22 +0100

    Add elevatable mixin

---
+++ src/mixins/Elevatable.ts
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the BibScan for Njuko package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+/**
+ * @author François Pluchino <francois.pluchino@gmail.com>
+ */
+@Component
+export class Elevatable extends Vue {
+    @Prop({type: [Number, String], default: null})
+    public elevation?: number;
+
+    public get computedElevation(): string|number|undefined {
+        return this.elevation;
+    }
+
+    public get elevationClasses(): Record<string, boolean> {
+        if (!this.computedElevation) {
+            return {};
+        }
+
+        return {
+            [`elevation-${this.computedElevation}`]: true,
+        };
+    }
+}
