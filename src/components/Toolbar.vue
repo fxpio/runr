e5d5d4173be40db5c0d67f1966435ca475cfbc94
François Pluchino <francois.pluchino@gmail.com>
2019-02-02 12:24:45 +0100

    Add transistion on the app toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -8,46 +8,48 @@ file that was distributed with this source code.
 -->
 
 <template>
-  <v-toolbar dark :color="$store.state.darkMode.enabled ? null : 'primary'" app clipped-left>
-    <v-scale-transition mode="out-in">
-      <v-toolbar-side-icon v-if="!showPreviousButton" @click="drawerButtonAction" key="menu-btn"></v-toolbar-side-icon>
+  <v-fade-transition>
+    <v-toolbar dark :color="$store.state.darkMode.enabled ? null : 'primary'" app clipped-left>
+      <v-scale-transition mode="out-in">
+        <v-toolbar-side-icon v-if="!showPreviousButton" @click="drawerButtonAction" key="menu-btn"></v-toolbar-side-icon>
 
-      <v-btn icon v-else @click="previousButtonAction" @long-click="drawerButtonAction" key="previous-btn">
-        <v-icon>arrow_back</v-icon>
-      </v-btn>
-    </v-scale-transition>
+        <v-btn icon v-else @click="previousButtonAction" @long-click="drawerButtonAction" key="previous-btn">
+          <v-icon>arrow_back</v-icon>
+        </v-btn>
+      </v-scale-transition>
 
-    <v-menu>
-      <v-toolbar-title slot="activator">
-        <v-fade-transition mode="out-in">
-          <div v-if="!$store.state.edition.serverPending">
-            <span>{{ toolbarTitle }}</span>
-            <v-icon dark>arrow_drop_down</v-icon>
-          </div>
-          <v-progress-circular indeterminate v-if="$store.state.edition.serverPending"></v-progress-circular>
-        </v-fade-transition>
-      </v-toolbar-title>
+      <v-menu>
+        <v-toolbar-title slot="activator">
+          <v-fade-transition mode="out-in">
+            <div v-if="!$store.state.edition.serverPending">
+              <span>{{ toolbarTitle }}</span>
+              <v-icon dark>arrow_drop_down</v-icon>
+            </div>
+            <v-progress-circular indeterminate v-if="$store.state.edition.serverPending"></v-progress-circular>
+          </v-fade-transition>
+        </v-toolbar-title>
 
-      <v-list>
-        <v-list-tile
-                v-for="edition in $store.state.edition.all"
-                :key="edition.id"
-                @click="$store.dispatch('edition/select', edition.id)"
-        >
-          <v-list-tile-content>
-            <v-list-tile-title v-text="edition.name"></v-list-tile-title>
-          </v-list-tile-content>
-          <v-list-tile-action>
-            <v-scale-transition>
-              <v-icon v-if="$store.getters['edition/isSelected'](edition.id)" color="pink">star</v-icon>
-            </v-scale-transition>
-          </v-list-tile-action>
-        </v-list-tile>
+        <v-list>
+          <v-list-tile
+                  v-for="edition in $store.state.edition.all"
+                  :key="edition.id"
+                  @click="$store.dispatch('edition/select', edition.id)"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title v-text="edition.name"></v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-scale-transition>
+                <v-icon v-if="$store.getters['edition/isSelected'](edition.id)" color="pink">star</v-icon>
+              </v-scale-transition>
+            </v-list-tile-action>
+          </v-list-tile>
 
-        <v-list-tile key="add" :to="{name: 'editions-add', query: {redirect: $router.currentRoute.fullPath}}">{{ $t('add.edition') }}</v-list-tile>
-      </v-list>
-    </v-menu>
-  </v-toolbar>
+          <v-list-tile key="add" :to="{name: 'editions-add', query: {redirect: $router.currentRoute.fullPath}}">{{ $t('add.edition') }}</v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+  </v-fade-transition>
 </template>
 
 <script lang="ts">
